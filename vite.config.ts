de7ffc7827aa1b77b89ae22470d7adf52dc2af00
ReshadAhmadi010 <reshadahmadi2001@gmail.com>
2025-08-10 14:52:34 +0430

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,5 @@
 import { defineConfig } from "vite";
-import react from "../vite-project/node_modules/@vitejs/plugin-react-swc/index.cjs";
-// import tailwindcss from "tailwindcss";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [tailwindcss()],
 });
